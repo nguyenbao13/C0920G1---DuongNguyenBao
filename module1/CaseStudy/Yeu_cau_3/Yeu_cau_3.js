28d7da3ce889c128ec79ae6f4ac56f02e3f4f8d7
nguyenbao13 <dnbaobk@gmail.com>
2020-10-05 16:45:36 +0700

    up

--- module1/CaseStudy/Yeu_cau_3/Yeu_cau_3.js
+++ module1/CaseStudy/Yeu_cau_3/Yeu_cau_3.js
@@ -9,17 +9,16 @@ let amount = document.getElementById('amount');
 let rentDay = document.getElementById('rentDay');
 let service = document.getElementById('service');
 let room = document.getElementById('room');
-
+//Kiểm tra giá trị nhập vào đúng định dạng chưa
 function checkValue() {
-do {
-    let check = false;
+    //Kiểm tra email
     let countAt = 0;
     let countDot = 0;
-    for (let i = 0; i < email.length; i++) {
-        if (email.charAt(i) === '@') {
+    for (let i = 0; i < email.value.length; i++) {
+        if (email.value.charAt(i) === '@') {
             countAt++;
-            for (let j = i + 1; j < email.length; j++) {
-                if (email.charAt(j) === '.') {
+            for (let j = i + 1; j < email.value.length; j++) {
+                if (email.value.charAt(j) === '.') {
                     countDot++;
                 }
             }
@@ -27,14 +26,24 @@ do {
     }
     if (countAt !== 1 || countDot < 1) {
         alert('Email bạn nhập không đúng');
-        break;
     } else {
-        check = true;
+        output();
     }
-} while (!check)
+    //Kiểm tra cmnd
+    if (!isNaN(idNum.value)) {
+        idNum.value = parseFloat(idNum.value);
+    }
+    if (!Number.isInteger(idNum)) {
+        alert('CMND không đúng định dạng');
+    }
+    if (idNum < 10000000 || idNum > 99999999) {
+        alert('CMND không đúng định dạng');
+    }
+    //Kiểm tra ngày sinh
+
 }
+//Xuất thông tin
 function output() {
-    checkValue();
     let ad;
     if (address.value == 'Đà Nẵng') {
         ad = 20;
@@ -79,16 +88,16 @@ function output() {
     let fee;
     fee = se * rd - ad - rdSale - cu;
     discount.value = ad + rdSale + cu;
-    document.getElementById('fee').innerHTML=fee;
-    document.getElementById('nameOut').innerHTML=name.value;
-    document.getElementById('idNumOut').innerHTML=idNum.value;
-    document.getElementById('birthdayOut').innerHTML=birthday.value;
-    document.getElementById('emailOut').innerHTML=email.value;
-    document.getElementById('addressOut').innerHTML=address.value;
-    document.getElementById('customerOut').innerHTML=customer.value;
-    document.getElementById('discountOut').innerHTML=discount.value;
-    document.getElementById('amountOut').innerHTML=amount.value;
-    document.getElementById('rentDayOut').innerHTML=rentDay.value;
-    document.getElementById('serviceOut').innerHTML=service.value;
-    document.getElementById('roomOut').innerHTML=room.value;
+    document.getElementById('fee').innerHTML = fee;
+    document.getElementById('nameOut').innerHTML = name.value;
+    document.getElementById('idNumOut').innerHTML = idNum.value;
+    document.getElementById('birthdayOut').innerHTML = birthday.value;
+    document.getElementById('emailOut').innerHTML = email.value;
+    document.getElementById('addressOut').innerHTML = address.value;
+    document.getElementById('customerOut').innerHTML = customer.value;
+    document.getElementById('discountOut').innerHTML = discount.value;
+    document.getElementById('amountOut').innerHTML = amount.value;
+    document.getElementById('rentDayOut').innerHTML = rentDay.value;
+    document.getElementById('serviceOut').innerHTML = service.value;
+    document.getElementById('roomOut').innerHTML = room.value;
 }
