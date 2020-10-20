35e7d9c2e2382863a1efa88fa3b30a5e86ae6e7b
nguyenbao13 <dnbaobk@gmail.com>
2020-10-20 16:07:47 +0700

    up

---
+++ module2/src/s1_introduction_to_java/thuc_hanh/phuong_trinh_bac_nhat/PhuongTrinhBacNhat.java
@@ -0,0 +1,7 @@
+package s1_introduction_to_java.thuc_hanh.phuong_trinh_bac_nhat;
+
+public class PhuongTrinhBacNhat {
+    public static void main(String[] args) {
+        
+    }
+}
--- module2/src/s1_introduction_to_java/thuc_hanh/th1/SystemTime.java
+++
@@ -1,5 +0,0 @@
-package s1_introduction_to_java.thuc_hanh.th1;
-
-public class SystemTime {
-    
-}
