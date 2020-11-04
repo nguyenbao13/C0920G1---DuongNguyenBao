40020551eb085cfad659770f20eebf24ce2f17ae
nguyenbao13 <dnbaobk@gmail.com>
2020-11-04 08:23:19 +0700

    up

--- module2/src/Demo.java
+++ module2/src/Demo.java
@@ -1,21 +1,15 @@
+import java.util.*;
+
 public class Demo {
-    int rollno;
-    String name;
-    static String college = "Bưu Chính Viễn Thông";
-
-    Demo(int r, String n) {
-        rollno = r;
-        name = n;
-    }
-
-    void display() {
-        System.out.println(rollno + " - " + name + " - " + college);
-    }
     public static void main(String[] args) {
-        Demo s1 = new Demo(111, "Thông");
-        Demo s2 = new Demo(222, "Minh");
-
-        s1.display();
-        s2.display();
+        List<String>	linkedList	=	new	LinkedList<>();
+        linkedList.add("New	York");
+        linkedList.add("Atlanta");
+        linkedList.add("Dallas");
+        linkedList.add("Madison");
+        ListIterator<String> listIterator = linkedList.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + "  ");
+        }
     }
 }
