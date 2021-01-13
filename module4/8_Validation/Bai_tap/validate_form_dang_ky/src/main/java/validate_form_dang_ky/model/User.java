5b152373b70fa91644b63a6488cc571064e431ce
nguyenbao13 <you@example.com>
2021-01-13 10:45:25 +0700

    up

---
+++ module4/8_Validation/Bai_tap/validate_form_dang_ky/src/main/java/validate_form_dang_ky/model/User.java
@@ -0,0 +1,9 @@
+package validate_form_dang_ky.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private int age;
+    private String email;
+}
